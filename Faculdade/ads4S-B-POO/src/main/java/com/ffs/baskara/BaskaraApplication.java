5c5b8203e7195e7c192272eeee57ec3493c64a02
Felipe Ferrari Da Silva <123752940+FelipeFerrariSilva@users.noreply.github.com>
2026-05-09 15:00:00 -0300

    s

---
+++ Faculdade/ads4S-B-POO/src/main/java/com/ffs/baskara/BaskaraApplication.java
@@ -0,0 +1,13 @@
+package com.ffs.baskara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaskaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaskaraApplication.class, args);
+	}
+
+}
